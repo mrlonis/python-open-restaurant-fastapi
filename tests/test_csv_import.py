f901cb425aab9f11c098fce54b5aff4c6566fe99
Matthew L <mattlonis@gmail.com>
2022-12-16 12:50:28 -0500

    refactor(): Refactoring csv import

--- tests/test_csv_import.py
+++ tests/test_csv_import.py
@@ -2,5 +2,5 @@ from app.utils.csv_utils import csv_import
 
 
 def test_csv_import():
-    restaurants = csv_import()
+    restaurants = csv_import("alembic/restaurants.csv")
     assert len(restaurants) == 276
