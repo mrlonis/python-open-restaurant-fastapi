fa08e98b14876a9b4b34b8beed1346c87e149580
Matthew Lonis <mattlonis@gmail.com>
2023-02-13 14:56:14 -0600

    Testing pylint ensure venv

--- tests/test_csv_import.py
+++ tests/test_csv_import.py
@@ -3,4 +3,5 @@ from app.utils.csv_utils import csv_import
 
 def test_csv_import():
     restaurants = csv_import("alembic/restaurants.csv")
+    assert restaurants is not None
     assert len(restaurants) == 276
