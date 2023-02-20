e2e717c723ef331623b11d1f8ec844a08d199612
Matthew Lonis <mattlonis@gmail.com>
2023-02-20 02:31:00 -0500

    Updating submodule

--- pip_compile.sh
+++ pip_compile.sh
@@ -2,3 +2,4 @@
 pip-compile -U --resolver=backtracking --strip-extras requirements.in
 pip-compile -U --resolver=backtracking --strip-extras requirements-test.in
 pip-compile -U --resolver=backtracking requirements-dev.in
+pip-sync requirements.txt requirements-test.txt requirements-dev.txt
